f5bd3ffa4dc9795d6e4a5ecab1a39b10c847ff76
ParamSachdeva <psparamjeet21@gmail.com>
2018-03-27 22:03:05 +0530

    ntialization

--- src/test/java/step_definitions/RunCukesTest.java
+++ src/test/java/step_definitions/RunCukesTest.java
@@ -9,9 +9,59 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "classpath:features",
-		plugin = {"pretty", "html:target/cucumber-html-report"},
+		plugin = {
+				"pretty", "html:target/cucumber-html-report",
+				"json:target/cucumber.json", 
+				"pretty:target/cucumber-pretty.txt",
+				"usage:target/cucumber-usage.json", 
+				"junit:target/cucumber-results.xml",
+				"pretty:target/cucumber-results.html"
+		},
 		tags = {}
 		)
 public class RunCukesTest{
 	
 }
+
+
+/*package step_definitions;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "classpath:features",
+		plugin = {"pretty", "html:target/cucumber-html-report"},
+		tags = {}
+		)
+ 
+
+@ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
+retryCount = 3,
+detailedReport = true,
+detailedAggregatedReport = true,
+overviewReport = true,
+//coverageReport = true,
+jsonUsageReport = "target/cucumber-usage.json",
+usageReport = true,
+toPDF = true,
+outputFolder = "target")
+
+
+@CucumberOptions(plugin = {
+		"html:target/cucumber-html-report",
+		"json:target/cucumber.json", 
+		"pretty:target/cucumber-pretty.txt",
+		"usage:target/cucumber-usage.json", 
+		"junit:target/cucumber-results.xml"
+		},
+tags = {"@Test"})
+public class RunCukesTest{
+
+}*/
